f24fbe6bcf7a1644cbcd17320035b7dee6537039
titoo9201 <titoos675@gmail.com>
2025-05-12 17:37:43 +0530

    Fix import case sensitivity for Usercontext

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Usercontext from './context/usercontext.jsx'
+import Usercontext from './context/Usercontext.jsx';
+
+
 
 
 
